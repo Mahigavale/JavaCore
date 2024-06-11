c05880f536fdb6d75eba7a3fe3a76b9726736b15
Mahigavale <maheshgavale07@gmail.com>
2024-06-11 12:02:44 +0530

    the commit

---
+++ src/Revison/Core/Casting.java
@@ -0,0 +1,22 @@
+package Revison.Core;
+
+public class Casting {
+
+	public static void main(String[] args) {
+		
+		
+		float i;
+		
+		i=67;
+		
+		System.out.println(i);
+		
+		
+		int j;
+		j=(int) 67.78;
+		
+		System.out.println(j);
+
+	}
+
+}

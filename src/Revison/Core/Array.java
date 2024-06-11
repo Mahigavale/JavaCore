16f14bfd65321b7922c7fadfb265bcc504466d52
Mahigavale <maheshgavale07@gmail.com>
2024-06-11 12:15:36 +0530

    array

--- src/Revison/Core/Array.java
+++ src/Revison/Core/Array.java
@@ -1,10 +1,29 @@
 package Revison.Core;
 
+import java.util.Scanner;
+
 public class Array {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		int[] demo=new int[3];
+		int[] demo2= {45,78,98};
+		 Scanner sc=new Scanner(System.in);
+		
+		for(int i=0;i<demo.length;i++)
+		{
+			System.out.println("please enter the"+i +"element");
+			demo[i]=sc.nextInt();
+		}
+   
+		//System.out.println(demo);
+	
 
+	
+	for(int j:demo)
+	{
+		System.out.print(j+" ");
 	}
-
+}
 }
